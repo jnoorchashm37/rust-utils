71ae28353becc408485eb52e4bcc281552a96946
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-05-16 17:14:55 -0400

    name change

--- src/or_else_async.rs
+++ src/or_else_async.rs
@@ -1,10 +1,10 @@
 #[allow(async_fn_in_trait)]
 pub trait OrElseAsync<T> {
-    async fn async_unwrap_or_else(self, f: impl AsyncFnOnce() -> T) -> T;
+    async fn unwrap_or_else_async(self, f: impl AsyncFnOnce() -> T) -> T;
 }
 
 impl<T> OrElseAsync<T> for Option<T> {
-    async fn async_unwrap_or_else(self, f: impl AsyncFnOnce() -> T) -> T {
+    async fn unwrap_or_else_async(self, f: impl AsyncFnOnce() -> T) -> T {
         match self {
             Some(t) => t,
             None => f().await,
@@ -13,7 +13,7 @@ impl<T> OrElseAsync<T> for Option<T> {
 }
 
 impl<T, E> OrElseAsync<T> for Result<T, E> {
-    async fn async_unwrap_or_else(self, f: impl AsyncFnOnce() -> T) -> T {
+    async fn unwrap_or_else_async(self, f: impl AsyncFnOnce() -> T) -> T {
         match self {
             Ok(t) => t,
             Err(_) => f().await,
@@ -28,18 +28,18 @@ mod tests {
     #[tokio::test]
     async fn test_option() {
         let a = Some(0);
-        assert_eq!(0, a.async_unwrap_or_else(async || 10000).await);
+        assert_eq!(0, a.unwrap_or_else_async(async || 10000).await);
 
         let b = None;
-        assert_eq!(10000, b.async_unwrap_or_else(async || 10000).await);
+        assert_eq!(10000, b.unwrap_or_else_async(async || 10000).await);
     }
 
     #[tokio::test]
     async fn test_result() {
         let a: Result<i32, ()> = Ok(0);
-        assert_eq!(0, a.async_unwrap_or_else(async || 10000).await);
+        assert_eq!(0, a.unwrap_or_else_async(async || 10000).await);
 
         let b: Result<i32, ()> = Err(());
-        assert_eq!(10000, b.async_unwrap_or_else(async || 10000).await);
+        assert_eq!(10000, b.unwrap_or_else_async(async || 10000).await);
     }
 }
